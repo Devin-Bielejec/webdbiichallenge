3695c91093187edb2e83a321824b45426dcb856a
devin-bielejec <devin.bieleje@gmail.com>
2019-09-19 14:59:27 -0400

    Get request complete

--- server.js
+++ server.js
@@ -5,9 +5,16 @@ const server = express();
 
 const port = 5000;
 
+const serverError = (res, error) => {
+    return res.status(500).json({message: error})
+}
 
 //read == get cars
-server.get("/api/cars", )
+server.get("/api/cars", (req, res) => {
+    db("cars")
+    .then(cars => res.json(cars))
+    .catch(error => serverError(res, error))
+})
 
 //create == post a car
 
