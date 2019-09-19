fb117b2e752b718dc92a0bdbdc41abc3a5c3c753
devin-bielejec <devin.bieleje@gmail.com>
2019-09-19 15:08:51 -0400

    successful get and post end points

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ const db = require("./data/dbconfig");
 
 const server = express();
 
+server.use(express.json());
+
 const port = 5000;
 
 const serverError = (res, error) => {
-    return res.status(500).json({message: error})
+    return res.status(500).json({message: `Error: error`})
+}
+
+const required = (res, item) => {
+    console.log("item", item);
+    !item ? res.status(404).json({message: `${item} is required.`}) : null;
 }
 
 //read == get cars
@@ -17,7 +24,17 @@ server.get("/api/cars", (req, res) => {
 })
 
 //create == post a car
-
+server.post("/api/cars", (req, res) => {
+    const { vin, make, model, mileage, transmission, title_status } = req.body;
+    required(res, vin);
+    required(res, make);
+    required(res, model);
+    required(res, mileage);
+    console.log(vin, make, model, mileage);
+    db("cars").insert({vin, make, model, mileage})
+    .then(cars => res.json(cars))
+    .catch(error => serverError(res, error))
+})
 
 
 server.listen(port, () => console.log(`Listening on port ${port}`))
